7adc8da769fb08680a430fea670a5d75a26832f7
GABRIEL.PALOZI <160649242+Gabrielpalozi778@users.noreply.github.com>
2025-02-13 08:53:22 -0300

    Create eeeeeeeeee.php

---
+++ eeeeeeeeee.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+</head>
+<body>
+    
+    <h1>AULA 02 INTRODUTÓRIA DE PHP</h1>
+    <h2>Prof. Anderson Vanin</h2>
+
+    <p>Oi sou o prof Anderson</p>
+    <p>Oi sou o prof Anderson</p><p>Oi sou o prof Anderson </p><p>Oi sou o prof Anderson</p>    
+</body>
+</html>
