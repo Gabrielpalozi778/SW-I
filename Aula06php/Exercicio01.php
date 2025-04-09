b630f530823d815d551843b584d92a352b1adaa4
JoseAlves-python <mestre.do.python16@gmail.com>
2025-04-09 09:10:19 -0300

    aaa

---
+++ Aula06php/Exercicio01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio01</title>
+</head>
+<body>
+ <html>
+    <ul>
+      <?php
+        for ($i=1; $i <= 10 ; $i++) { 
+            echo "<li>item $i</li>";
+        }    
+      ?>
+    </ul>   
+
+ </html>
+  
+</body>
+</html>
